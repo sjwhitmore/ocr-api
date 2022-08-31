3e817ace982618361c633ea053de3187bd920653
Samantha Whitmore <whitmore.samantha@gmail.com>
2022-08-31 12:49:22 -0700

    try a bit less complex preprocessing

--- ocr.py
+++ ocr.py
@@ -16,26 +16,26 @@ def ocr():
        image_url,
        img_path)
     # load the example image and convert it to grayscale
-    image = cv2.imread(img_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # apply thresholding to preprocess the image
-    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-    # apply median blurring to remove any blurring
-    gray = cv2.medianBlur(gray, 3)
-
-    # save the processed image in the /static/uploads directory
-    proc_path = 'proc_' + img_path
-    cv2.imwrite(proc_path, gray)
+    # image = cv2.imread(img_path)
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #
+    # # apply thresholding to preprocess the image
+    # gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    #
+    # # apply median blurring to remove any blurring
+    # gray = cv2.medianBlur(gray, 3)
+    #
+    # # save the processed image in the /static/uploads directory
+    # proc_path = 'proc_' + img_path
+    # cv2.imwrite(proc_path, gray)
 
     # perform OCR on the processed image
     config = ('-l eng --oem 1 --psm 3')
-    raw_result = pytesseract.image_to_string(Image.open(proc_path), config=config)
+    raw_result = pytesseract.image_to_string(Image.open(img_path), config=config)
     # it always gets "I" wrong
     result = raw_result.replace('|', 'I')
     # remove the processed image
-    os.remove(proc_path)
+    #os.remove(proc_path)
     # remove original image
     os.remove(img_path)
     return {
